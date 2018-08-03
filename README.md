97e3f558214bd8633324adb71676f458504a8548
Jon Akers <jka@hpc.ufl.edu>
2018-08-03 14:52:46 -0400

    First stage.
    Currently file outputs to screen the scrambled file.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# rotfu
-ROT Experiment
+Introduction:
+
+rotfu takes the old ROT13 function and plays with it. Instead of simply rotating the alphabet 13 characters,
+it will instead progressively rotate the alphabet, such that the first character of the alphabet it
+encounters will be incremented by one letter (i.e. "C" becomes "D"), then the next will increment two ("C"
+becomes "E"), etc. The same will happen for numerals under a different counter. Punctuation characters will
+remain the same.
+
+Caveats:
+
+If a binary file is fed into rotfu, it will error out declaring that the file cannot be converted.
---
+++ rotfu.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  // Check for input
+  if (argc != 2) {
+    printf("Must provide an input file\n");
+    exit(0);
+  }
+
+  // Check if there is a file
+  struct stat fileStat;
+  if (stat(argv[1], &fileStat) < 0)
+    return 1;
+
+  // Check if the file is a regular file, i.e. not a directory, etc.
+  if (!S_ISREG(fileStat.st_mode)) {
+    printf("Not regular file\n");
+    exit(0);
+  }
+  
+  // Create output filename
+  char outfile[strlen(argv[1]) + 10];
+  strcpy(outfile, argv[1]);
+  strcat(outfile, ".rfu");
+
+  // Open input and output files
+  FILE *fp;
+  fp=fopen(argv[1], "r");
+  FILE *fp2;
+  fp2=fopen(outfile, "w");
+
+  int c,a,n,cr;
+  a=0;
+  n=0;
+  if(fp == NULL) {
+    perror("Error in opening file");
+    return(-1);
+  } do {
+    c = fgetc(fp);
+    cr=c;
+    if (feof(fp)) {
+      break;
+    }
+    if(c > 47 && c<58) {
+      n++;
+      cr=c+n;
+      while(cr>57) {
+	cr-=10;
+      }
+    } else if (c > 64 && c < 91) {
+      a++;
+      cr=c+a;
+      while(cr>90) {
+	cr-=26;
+      }
+    } else if (c>96 && c<123) {
+      a++;
+      cr=c+a;
+      while(cr>122) {
+	cr-=26;
+      }
+    } 
+    printf("%c", cr);
+  } while(1);
+
+  fclose(fp);
+  fclose(fp2);
+  return(0);
+}
